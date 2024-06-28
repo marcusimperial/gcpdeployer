b022bfe4da157f4ac53db9cb722db0f89cb7bfac
Marcus Imperial <marcus.imperial01@gmail.com>
2024-06-28 18:26:39 +0800

    test

--- index.mjs
+++ index.mjs
@@ -46412,7 +46412,7 @@ function validateCookieMaxAge (maxAge) {
 
 /**
  * @see https://www.rfc-editor.org/rfc/rfc6265#section-4.1.1
- * @param {import('./index').Cookie} cookie
+ * @param {import('./index.mjs').Cookie} cookie
  */
 function stringify (cookie) {
   if (cookie.name.length === 0) {
