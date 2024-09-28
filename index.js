9619eb47fd78b5ccd7ff792e729f64d0c9b2163d
Marcus Imperial <marcus.imperial01@gmail.com>
2024-09-28 13:54:48 +0800

    reverted: static handler changes

--- index.js
+++ index.js
@@ -65705,7 +65705,7 @@ const checkFileExists = async (path = '') => {
 
 const readDirectory = async (dirPath) => {
     try {
-        const files = await (0,promises_namespaceObject.readdir)(dirPath);
+        const files = await readdir(dirPath);
         console.log(files);
         return files;
     } catch (error) {
@@ -66407,8 +66407,6 @@ const updateStaticFrontend = async (folderPath, websiteName, location, config) =
 
     (0,core.info)('STEP 1 of 4: Beginning JSON Config retrieval attempt...');
     let fileName = `${generateId()}.zip`;
-    readDirectory('/home/runner/work/chrome-extension/chrome-extension/dist');
-    readDirectory('/home/runner/work/chrome-extension/dist');
     const packageConfig = await readJSONFile(`${folderPath}/config.json`);
     if (!packageConfig) return (0,core.info)('No JSON config file found, using generic file name instead.');
     else fileName = `${packageConfig?.name}-${packageConfig?.version}.zip`;
